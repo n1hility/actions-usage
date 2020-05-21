91331090fee3bc816fcff3a5d12b428dfebb3a92
Jason T. Greene <jason.greene@redhat.com>
2020-05-20 20:52:33 -0500

    Add release script

---
+++ build-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NAME=actions-usage
+PLATFORMS="linux darwin windows"
+VERSION=0.1.0
+
+go get
+for i in $PLATFORMS; do
+  rm -rf target/$i; mkdir -p target/$i/$NAME
+  GOOS=$i go build -o target/$i/$NAME
+  (cd target/$i; tar czvf $NAME-$VERSION.tar.gz; zip $NAME-$VERSION.zip -r $NAME)
+done;
